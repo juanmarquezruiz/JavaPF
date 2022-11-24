6ce4145b8f8172ddfe8b622cbf1da6e44b2e8924
Juan Marquez <Juanmarquezr@gmail.com>
2022-11-23 19:03:41 -0500

    Version 1 para sustentar

--- src/proyectofinalpoo/ConexionMySQL.java
+++ src/proyectofinalpoo/ConexionMySQL.java
@@ -1,35 +1,21 @@
 package proyectofinalpoo;
-
 import java.sql.*;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ConexionMySQL {
-
-    // La conexión a la BD
     private static Connection con = null;
-
-    // Las diversas instrucciones que se envían a la BD
     private static Statement st = null;
-
-    // El driver de MySQL
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-
-    // El url de la BD, se especifica el nombre del host y el de la base de datos
-    // private static final String URL = "jdbc:mysql://localhost:3306/subsidios2022";
     private static final String URL = "jdbc:mysql://u3izbkwgfact1l0c:1DwgeE2VixHUgIuVAY8O@bt5vtvpi1cmv4s5i4co8-mysql.services.clever-cloud.com:3306/bt5vtvpi1cmv4s5i4co8";
 
-    // Abrir la conexión con la BD
     public static void abrirConexion() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
-        // Se realiza la conexión
         Class.forName(DRIVER);
         con = DriverManager.getConnection(URL, "u3izbkwgfact1l0c", "1DwgeE2VixHUgIuVAY8O");
         st = con.createStatement();
     }
 
-    // Cerrar la conexiòn con la BD
     public static void cerrarConexion() throws SQLException {
         con.close();
         st.close();
@@ -38,22 +24,112 @@ public class ConexionMySQL {
     }
 
     public static LinkedList<Subsidiado> listarSubsidiado() throws SQLException {
-            ResultSet rs;         // Para obtener los datos de la BD
+            ResultSet rs;  
+            String consulta;
+            LinkedList<Subsidiado> lst = new LinkedList();         
+        try {
+            abrirConexion();
+            consulta = "SELECT * FROM subsidiado "
+                     + "where Subsidio <> 'No subsidiado';";
+            rs = st.executeQuery(consulta);
+            while (rs.next()) {
+                int no = rs.getInt("NO");
+                int identificacion = rs.getInt("Identificacion");
+                String Municipio = rs.getString("Municipio");
+                String Departamento = rs.getString("Departamento");
+                String Subsidio = rs.getString("Subsidio");
+                String Nombre = rs.getString("Nombre"); 
+                Subsidiado p = new Subsidiado(no, identificacion, Municipio, Departamento, Subsidio, Nombre);
+                lst.add(p); 
+            }
+            rs.close();
+            cerrarConexion();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+            Logger.getLogger(ConexionMySQL.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        return lst;
+    }
+    
+    public static LinkedList<Subsidiado> BuscarSubsidiado(long Identificacion) throws SQLException {
+            ResultSet rs;  
+            String consulta;
+            LinkedList<Subsidiado> lst = new LinkedList();
+        try {
+            abrirConexion();
+            consulta = "SELECT * FROM subsidiado "
+                    + "where Identificacion = "+ Identificacion +";";
+            rs = st.executeQuery(consulta);
+            while (rs.next()) {
+                int no = rs.getInt("NO");
+                int identificacion = rs.getInt("Identificacion");
+                String Municipio = rs.getString("Municipio");
+                String Departamento = rs.getString("Departamento");
+                String Subsidio = rs.getString("Subsidio");
+                String Nombre = rs.getString("Nombre"); 
+                Subsidiado p = new Subsidiado(no, identificacion, Municipio, Departamento, Subsidio, Nombre);
+                lst.add(p); 
+            }
+            rs.close();
+            cerrarConexion();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+            Logger.getLogger(ConexionMySQL.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        return lst;
+    }
+    
+    public static LinkedList<Subsidio> CatSubsidio() throws SQLException {
+        ResultSet rs;        
+        String consulta;
+        LinkedList<Subsidio> lst = new LinkedList();
+        consulta = "SELECT * FROM subsidio;";
+        try {
+            abrirConexion();
+            rs = st.executeQuery(consulta);
+            while (rs.next()) {
+                int Id = rs.getInt("Id");
+                String SubsidioNombre = rs.getString("SubsidioNombre");
+                Subsidio p = new Subsidio(Id, SubsidioNombre);
+                lst.add(p); 
+            }
+            rs.close();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+            Logger.getLogger(ConexionMySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lst;
+    }
+    
+    public static String validarSubsidiado(long IdentificacionSubsidiado) {
+        try {
+            abrirConexion();
+            ResultSet rs;
+            String Query_Validacion = "Select identificacion from subsidiado where identificacion = " + IdentificacionSubsidiado + ";";
+            rs = st.executeQuery(Query_Validacion);
+            if (rs.next() == false) {
+                return "no existe";
+            } else {
+                return "Si existe";
+            }
+        } catch (SQLException e) {
+            return "Error, ocurrio un problema en la BD";
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+            Logger.getLogger(ConexionMySQL.class.getName()).log(Level.SEVERE, null, ex);
+            return ex.getMessage();
+        }
+    }
+    
+    public static LinkedList<Subsidiado> listarCiudadano() throws SQLException {
+            ResultSet rs;  
             String consulta;
             LinkedList<Subsidiado> lst = new LinkedList();
             
         try {
             
             abrirConexion();
-      
-
-            // Se arma la consulta
-            consulta = "SELECT s.*, c.nombre FROM subsidiado s inner join ciudadano c on c.identificacion = s.identificacion;";
-
-            // Se envia la consulta a la BD
+            consulta = "SELECT * FROM subsidiado ;";          
+                    
             rs = st.executeQuery(consulta);
-
-            // Ahora se obtiene la información de la BD
             while (rs.next()) {
                 int no = rs.getInt("NO");
                 int identificacion = rs.getInt("Identificacion");
@@ -62,175 +138,30 @@ public class ConexionMySQL {
                 String Subsidio = rs.getString("Subsidio");
                 String Nombre = rs.getString("Nombre"); 
                
-
                 Subsidiado p = new Subsidiado(no, identificacion, Municipio, Departamento, Subsidio, Nombre);
-                lst.add(p); // Se agrega el objeto a la lista de estudiantes
+                lst.add(p); 
             }
-
-            // Se cierra el ResultSet
             rs.close();
             cerrarConexion();
-
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
             Logger.getLogger(ConexionMySQL.class.getName()).log(Level.SEVERE, null, ex);
             return null;
         }
-
         return lst;
     }
-
-    public static LinkedList<Subsidio> listarSubsidio() throws SQLException {
-        ResultSet rs;         // Para obtener los datos de la BD
-        String consulta;
-        LinkedList<Subsidio> lst = new LinkedList();
-
-        // Se arma la consulta
-        consulta = "SELECT * FROM subsidio;";
-
-        try {
-            abrirConexion();
-            // Se envia la consulta a la BD
-            rs = st.executeQuery(consulta);
-
-            // Ahora se obtiene la información de la BD
-            while (rs.next()) {
-                int Id = rs.getInt("Id");
-                String SubsidioNombre = rs.getString("SubsidioNombre");
-
-                Subsidio p = new Subsidio(Id, SubsidioNombre);
-                lst.add(p); // Se agrega el objeto a la lista de estudiantes
-            }
-
-            // Se cierra el ResultSet
-            rs.close();
-
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-            Logger.getLogger(ConexionMySQL.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return lst;
-    }
-
-    public static String validarSubsidiado(int IdentificacionSubsidiado) {
-
-        try {
-
-            abrirConexion();
-
-            ResultSet rs;
-
-            String Query_Validacion = "Select identificacion from ciudadano where identificacion = " + IdentificacionSubsidiado + ";";
-
-            rs = st.executeQuery(Query_Validacion);
-
-            if (rs.next() == false) {
-                return "no existe";
-            } else {
-                return "Si existe";
-            }
-        } catch (SQLException e) {
-
-            return "Error, ocurrio un problema en la BD";
-
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-            Logger.getLogger(ConexionMySQL.class.getName()).log(Level.SEVERE, null, ex);
-            return ex.getMessage();
-        }
-    }
-
-    public static LinkedList<Ciudadano> listarCiudadano() throws SQLException {
-        ResultSet rs;
-        String consulta;
-        LinkedList<Ciudadano> lst = new LinkedList();
-
-        consulta = "SELECT * FROM ciudadano;";
-
-        // Se envia la consulta a la BD
-        rs = st.executeQuery(consulta);
-        while (rs.next()) {
-            int identificacion = rs.getInt("Identificacion");
-            String nombre = rs.getString("Nombre");
-            Ciudadano c = new Ciudadano(identificacion, nombre);
-            lst.add(c); // Se agrega el objeto a la lista de estudiantes
-        }
-        rs.close();
-        return lst;
-    }
-
-    /* 
-            StringBuilder salida = new StringBuilder();
-        Iterator it = lst.iterator();
-        while (it.hasNext()) {
-
-       Ciudadano c = new Ciudadano();
-            boolean res = it.hasNext();
-            JOptionPane.showMessageDialog(null, "Identificacion: "+c.getIdentificacion()+"Nombre: "+ c.getNombre());
-            salida.append(res);
-        }
-        System.out.println(salida);
     
-    
-    
-     */
-    public static void imprimirLista(LinkedList<Subsidiado> lst) {
-        StringBuilder salida = new StringBuilder();
-        Iterator it = lst.iterator();
-        while (it.hasNext()) {
-            Subsidiado p = (Subsidiado) it.next();
-            String res = "no" + p.getNo()
-                    + "Identificacion: " + p.getIdentificacion()
-                    + "Municipio: " + p.getMunicipio()
-                    + "Departamento: " + p.getDepartamento()
-                    + "Subsidio: " + p.getSubsidio()
-                    + "\n";
-            salida.append(res);
-        }
-        System.out.println(salida);
-    }
-
-    public static String insertarpersona(int Identificacion, String Nombre) throws SQLException {
+ public static String insertarsubsidio(long Identificacion, String Municipio,
+            String Departamento, String Subsidio, String Nombre) throws SQLException {
         try {
             abrirConexion();
-
-            String insertpersona = "INSERT INTO SUBSIDIO VALUES (" + Identificacion + "','" + Nombre + "');";
-            st.executeUpdate(insertpersona);
-
-            cerrarConexion();
-            return "Ok";
-
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-            Logger.getLogger(ConexionMySQL.class.getName()).log(Level.SEVERE, null, ex);
-            return "Error, " + ex.getMessage();
-        }
-
-    }
-
-    public static String insertarsubsidio(int Identificacion, String Municipio,
-            String Departamento, String Subsidio) throws SQLException {
-
-        try {
-            abrirConexion();
-            String insertsubsidio = "INSERT INTO SUBSIDIO VALUES "
-                    + "(" + Identificacion + "','" + Municipio + "', '" + Departamento + "','" + Subsidio + "');";
+            String insertsubsidio = "INSERT INTO subsidiado VALUES "
+                    + "( null, "+ Identificacion + ",'" + Municipio + "', '" + Departamento + "','" + Subsidio + "','"+ Nombre  +"');";
             st.executeUpdate(insertsubsidio);
-
             cerrarConexion();
             return "Ok";
-
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
             Logger.getLogger(ConexionMySQL.class.getName()).log(Level.SEVERE, null, ex);
             return "Error, " + ex.getMessage();
         }
-    }
-
-    //esto no debe estar en esta clase 
-    public static void buscarSubsidio(LinkedList<Subsidiado> identificacion) throws SQLException {
-        // Se arma la consulta
-        //LinkedList<Subsidiado> identificacion = Integer.parseInt(JOptionPane.showInputDialog("Introduzca la identificacion del subsidiado que va  buscar"));
-        //String consulta = "Select identificacion from subsidiados;";
-        //System.out.println(consulta);
-
-        // Se envia la consulta a la BD
-        //st.executeUpdate(consulta);
-    }
+    } 
 }
--- src/proyectofinalpoo/ProyectofinalPoo.java
+++ src/proyectofinalpoo/ProyectofinalPoo.java
@@ -1,214 +1,140 @@
 package proyectofinalpoo;
-
+import java.awt.HeadlessException;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class ProyectofinalPoo {
 
     static int select = -1;
-    static LinkedList<Subsidio> ListaSubsidio = new LinkedList();
 
     public static void main(String[] args) {
-
-        CargarListaSubsidios();
-
         while (select != 5) {
-
             try {
-                String Lectura = JOptionPane.showInputDialog(
-                        null,
-                        "Seleccione un opción:"
-                        + "\n1. Registrar Subsidio"
-                        + "\n2. Consultar Ciudadanos"
-                        + "\n3. Consultar Subsidiados"
-                        + "\n4. Buscar Subsidiados"
-                        + "\n5. Salir"
-                );
-
+                String Lectura = JOptionPane.showInputDialog(null, """
+                                                                   Seleccione un opci\u00f3n:
+                                                                   1. Registrar Subsidio
+                                                                   2. Consultar Ciudadanos
+                                                                   3. Consultar Subsidiados
+                                                                   4. Buscar Subsidiados
+                                                                   5. Salir""");
                 select = Integer.parseInt(Lectura);
-
                 switch (select) {
-
-                    case 1:
-
-                        JOptionPane.showMessageDialog(null, RegistraSubsidio());
-                        break;
-
-                    case 2:
-
-                        JOptionPane.showMessageDialog(null, ConsultarCiudadanos());
-                        break;
-
-                    case 3:
-
-                        JOptionPane.showMessageDialog(null, ConsultarSubsidiados());
-                        break;
-
-                    case 4:
-
-                        JOptionPane.showMessageDialog(null, BuscarSubsidiados());
-                        break;
-
-                    case 5:
-
-                        JOptionPane.showMessageDialog(null, "Gracias por usar el Software");
-                        break;
-
-                    default:
-
-                        JOptionPane.showMessageDialog(null, "Opcion no reconocida");
-                        break;
-
+                    case 1 -> JOptionPane.showMessageDialog(null, RegistraSubsidio());
+                    case 2 -> JOptionPane.showMessageDialog(null, ConsultarCiudadanos());
+                    case 3 -> JOptionPane.showMessageDialog(null, ConsultarSubsidiados());
+                    case 4 -> JOptionPane.showMessageDialog(null, BuscarSubsidiado());
+                    case 5 -> JOptionPane.showMessageDialog(null, "Gracias por usar el Software");
+                    default -> JOptionPane.showMessageDialog(null, "Opcion no reconocida");
                 }
-
-            } catch (Exception e) {
+            } catch (HeadlessException | NumberFormatException e) {
                 JOptionPane.showMessageDialog(null, "Uoop! Error!, si intenta salir use opcion 5.");
-
             }
         }
     }
-
+ 
     public static String RegistraSubsidio() {
         try {
+            LinkedList<Subsidio> CatSubsidios = CargarListaSubsidios();
+            String[] ListaSubsidios = ArrayString(CatSubsidios);
             String NombreSubsidiado;
-            int IdentificacionSubsidiado;
+            long IdentificacionSubsidiado;
             String Municipio;
             String Departamento;
             String Subsidio;
-            NombreSubsidiado = JOptionPane.showInputDialog("Introduzca Nombre Completo");
-            IdentificacionSubsidiado = Integer.parseInt(JOptionPane.showInputDialog("Introduzca su identificacion"));
-            Municipio = JOptionPane.showInputDialog("Introduzca Municipio");
-            Departamento = JOptionPane.showInputDialog("Introduzca Departamento");
-            Subsidio = JOptionPane.showInputDialog("Introduzca Subsidio");
-
+            NombreSubsidiado = JOptionPane.showInputDialog(null,"Introduzca Nombre Completo");
+            IdentificacionSubsidiado = Integer.parseInt(JOptionPane.showInputDialog(null,"Introduzca su identificacion"));
+            Municipio = JOptionPane.showInputDialog(null, "Introduzca Ciudad o Municipio");
+            Departamento = JOptionPane.showInputDialog(null, "Introduzca Departamento");
+            Subsidio = (String)JOptionPane.showInputDialog(null, "Seleccione Subsidio", "Subsidio", JOptionPane.QUESTION_MESSAGE, null, ListaSubsidios, ListaSubsidios[0]);
+            
             String Result = ConexionMySQL.validarSubsidiado(IdentificacionSubsidiado);
-
             if (!"no existe".equals(Result)) {
                 return Result;
             }
 
-            Result = ConexionMySQL.insertarpersona(IdentificacionSubsidiado, NombreSubsidiado);
+            Result = ConexionMySQL.insertarsubsidio(IdentificacionSubsidiado, Municipio, Departamento, Subsidio, NombreSubsidiado);
+            
             if (!"Ok".equals(Result)) {
                 return Result;
             }
-
-            Result = ConexionMySQL.insertarsubsidio(IdentificacionSubsidiado, Municipio, Departamento, Subsidio);
-            if (!"Ok".equals(Result)) {
-                return Result;
-            }
-
-            return "Subsidio del ciudadano registrado, y termino aca";
-
-        } catch (Exception e) {
-            return e.getMessage();
+            return "Subsidio del ciudadano registrado";
+        } catch (HeadlessException | NumberFormatException | SQLException e) {
+            
+            return "Opps! intente nuevamente. A ocurrido el siguiente error, " + e.getMessage();
         }
-
     }
-
+    
+       public static String[] ArrayString(LinkedList<Subsidio> list) {
+        String[] OutPutArray = new String[list.size()];
+        int i = 0;
+        for (Subsidio sub : list){
+           OutPutArray[i] = sub.getSubsidioNombre();
+           i++;
+       }
+        return OutPutArray;
+    }
+    
     public static String ConsultarCiudadanos() {
-
         try {
-            ConexionMySQL.abrirConexion();
-            //System.out.println("Se conectó a la base de datos");
-            LinkedList<Ciudadano> res = ConexionMySQL.listarCiudadano();
+            LinkedList<Subsidiado> ResultadoCiudadanos = ConexionMySQL.listarCiudadano();
             
-
-            ConexionMySQL.cerrarConexion();
-            imprimirCiudadano(res);
-            
-            //System.out.println("Se desconectó de la base de datos");
-        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException e) {
-            System.out.println("Excepción:" + e);
-        }
-        return "";
-    }
-
-       public static String ConsultarSubsidiados() {    
-        try {
-           
-            //System.out.println("Se conectó a la base de datos");
-            LinkedList<Subsidiado> ResultadoSubsidiados = ConexionMySQL.listarSubsidiado();
-            
-            return listarSubsidiado(ResultadoSubsidiados);
+            return GenerarLista(ResultadoCiudadanos);
            
         } catch (SQLException e) {
-           
             return "Excepción:" + e.getMessage();
-        
         }
-     
-    } 
+    }
     
- 
-    public static String BuscarSubsidiados() {    
- try {
-     
-            ConexionMySQL.abrirConexion();
-            //System.out.println("Se conectó a la base de datos");
-            LinkedList<Subsidiado> res = ConexionMySQL.listarSubsidiado();
-            ConexionMySQL.buscarSubsidio(res);
+    public static String ConsultarSubsidiados() {
+        try {
             
-            ConexionMySQL.cerrarConexion();
-            //System.out.println("Se desconectó de la base de datos");
-        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException e) {
+            LinkedList<Subsidiado> ResultadoSubsidiados = ConexionMySQL.listarSubsidiado();
+                    
+            return GenerarLista(ResultadoSubsidiados);
+            
+        } catch (SQLException e) {
+            return "Excepción:" + e.getMessage();
+        }
+    }
+    
+    public static String BuscarSubsidiado() {
+        try {
+            long IdentificacionSubsidiado = Integer.parseInt(JOptionPane.showInputDialog("Introduzca su identificacion"));
+            LinkedList<Subsidiado> ResultadoSubsidiados = ConexionMySQL.BuscarSubsidiado(IdentificacionSubsidiado);
+            return GenerarLista(ResultadoSubsidiados);
+        } catch (SQLException e) {
             System.out.println("Excepción:" + e);
         }
         return "";
     }
     
-    public static void CargarListaCiudadanos(){
-      try {
-          LinkedList<Ciudadano> ListaCiudadano = ConexionMySQL.listarCiudadano();
-        } catch (SQLException ex) {
-            Logger.getLogger(ProyectofinalPoo.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    }
-    
-
-    public static void CargarListaSubsidios() {
-
+    public static LinkedList<Subsidio> CargarListaSubsidios() {
         try {
-            ListaSubsidio = ConexionMySQL.listarSubsidio();
+            return ConexionMySQL.CatSubsidio();
         } catch (SQLException ex) {
-            Logger.getLogger(ProyectofinalPoo.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+            return null;
         }
-
     }
-
-    public static void imprimirCiudadano(LinkedList<Ciudadano> lst) {
-        StringBuilder salida = new StringBuilder();
+  
+    public static String GenerarLista(LinkedList<Subsidiado> lst) {
+       
         Iterator it = lst.iterator();
+        String Check = "\u2713";
+        String noCheck = "X";
+        String mark = "";
+        String Subsidio = "";
+         String ToString = "                               "+ Check + " = Tiene Subsidio, " + noCheck + " = Sin Subsidio, " +"\n" +"\n";
         while (it.hasNext()) {
-            
-            Ciudadano ObjCiudadano = (Ciudadano) it.next();
-            String res = "Identificacion: " + ObjCiudadano.getIdentificacion()
-                    + "Nombre: " + ObjCiudadano.getNombre()
-                    + "\n";
-            salida.append(res);
-        }
-        System.out.println(salida);
-    }
-    
-    
-    
-     public static String listarSubsidiado(LinkedList<Subsidiado> lst) {
-        String ToString =  "";
-         
-        Iterator it = lst.iterator();
-        
-        while (it.hasNext()) {
-            
             Subsidiado ObjSubsidiado = (Subsidiado) it.next();
-            ToString += ObjSubsidiado.getNo() + " Nombre: " + ObjSubsidiado.getNombre() + " Identificacion: " + ObjSubsidiado.getIdentificacion() 
-                    + " Departamento: " + ObjSubsidiado.getDepartamento() + " Municipio: " + ObjSubsidiado.getMunicipio() 
-                    + " Subsidio:" + ObjSubsidiado.getSubsidio() + "\n";
-            
+            Subsidio = ObjSubsidiado.getSubsidio().trim();
+            ToString += ("No subsidiado".equals(Subsidio) ? noCheck : Check) + " -- "
+                    + ObjSubsidiado.getNo() + " NOM: " + ObjSubsidiado.getNombre() + " DOC: " + ObjSubsidiado.getIdentificacion()
+                    + " DEP: " + ObjSubsidiado.getDepartamento() + " MUN: " + ObjSubsidiado.getMunicipio()
+                    + " SUBSIDIO: " + ObjSubsidiado.getSubsidio() + "\n";
         }
-       return ToString;
+        return ToString;
     }
 }
